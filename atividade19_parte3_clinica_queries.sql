3365780ddd2ad5d80218b663b43f98ee1a5c3286
CAMILA ALVES <camila_alves3@estudante.sesisenai.org.br>
2024-06-19 21:45:07 -0300

    clinica database

--- atividade19_parte3_clinica_queries.sql
+++ atividade19_parte3_clinica_queries.sql
@@ -99,3 +99,112 @@ where codm = 5;
 
 delete from consultas
 where codm = 5;
+
+/*  Buscar o nome e o CPF dos médicos com menos de 40 anos ou com especialidade
+diferente de traumatologia. */
+
+use clinica;
+select * from medicos;
+
+select nome, cpf, idade, especialidade
+from medicos
+where idade < 40 || especialidade != 'traumatologia';
+
+/* 2. Buscar todos os dados das consultas marcadas no período da tarde após
+o dia 19/06/2006 */
+
+select * from consultas;
+
+select * from consultas
+where data = '2006-06-19' and hora > '12:00';
+
+/* 3. Buscar o nome e a idade dos pacientes que não residem em Florianópolis. */
+
+select * from pacientes;
+
+select nome, idade, cidade
+from pacientes
+where cidade != 'Florianopolis';
+
+/* Buscar a hora das consultas marcadas antes do dia 14/06/2006 e depois
+do dia 20/06/2006 */
+
+select data, hora
+from consultas
+where data < '2006-06-14' || data > '2006-06-20';
+
+/* Buscar o nome e a idade (em meses) dos pacientes */
+select * from pacientes;
+
+select nome as paciente, idade * (12) as 'idade (em meses)'
+from pacientes
+order by nome;
+
+/* Em quais cidades residem os funcionários? */
+
+select nome as funcionario, cidade
+from funcionarios
+group by cidade
+order by cidade;
+
+select * from funcionarios;
+
+/* Qual o menor e o maior salário dos funcionários da Florianópolis? */
+
+select max(salario) as maior_salario,  min(salario) as menor_salario
+from funcionarios
+where cidade = 'Florianópolis'
+limit 1;
+
+/* Qual o horário da última consulta marcada para o dia 13/06/2006? */
+
+select data, hora
+from consultas
+where data = '2006-06-13'
+limit 1;
+
+/* Qual a média de idade dos médicos e o total de ambulatórios atendidos por eles? */
+
+select avg(idade) as media_idade, count(nroa) as total_ambulatorios
+from medicos;
+
+/* Buscar o código, o nome e o salário líquido dos funcionários. O salário líquido é
+obtido pela diferença entre o salário cadastrado menos 20% deste mesmo salário */
+
+select 
+	codf as codigo, 
+    nome as funcionario, 
+    salario as salario_bruto, 
+    round(salario * 0.80) as salario_liquido
+from funcionarios;
+
+/* Buscar o nome dos funcionários que terminam com a letra “a” */
+select nome 
+from funcionarios
+where nome like '%a'
+order by nome;
+
+/* Buscar o nome e CPF dos funcionários que não possuam a seqüência “00000”
+em seus CPFs */
+
+select nome, cpf
+from funcionarios
+where cpf like '00000%'
+order by nome;
+
+/* Buscar o nome e a especialidade dos médicos cuja segunda e a última letra de
+seus nomes seja a letra “o” */
+
+select nome, especialidade
+from medicos
+where nome like '_o%o'
+order by nome;
+
+/* Buscar os códigos e nomes dos pacientes com mais de 25 anos que estão com
+tendinite, fratura, gripe e sarampo */
+
+select codp as codigo, nome as paciente, idade, doenca
+from pacientes
+where doenca = 'tendinite' || 'fratura' || 'gripe' || 'sarampo' and idade > 25;
+
+select * from funcionarios;
